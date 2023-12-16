5d64860164459c037e6593de1a2f4d07a1c30352
Karine Voskanyan <153293656+missVoskaniann@users.noreply.github.com>
2023-12-16 19:03:49 +0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 int main() {
     BST bst;
 
-    // Insert values into the BST
     bst.insert(50);
     bst.insert(30);
     bst.insert(70);
@@ -13,22 +12,18 @@ int main() {
     bst.insert(60);
     bst.insert(80);
 
-    // Print inorder traversal
     std::cout << "Inorder Traversal: ";
     bst.inorderTraversal();
     std::cout << std::endl;
 
-    // Print preorder traversal
     std::cout << "Preorder Traversal: ";
     bst.preorderTraversal();
     std::cout << std::endl;
 
-    // Print postorder traversal
     std::cout << "Postorder Traversal: ";
     bst.postorderTraversal();
     std::cout << std::endl;
 
-    // Search for a value in the BST
     int searchValue = 40;
     Node* searchResult = bst.search(searchValue);
     if (searchResult) {
@@ -37,17 +32,14 @@ int main() {
         std::cout << searchValue << " not found in the BST." << std::endl;
     }
 
-    // Get minimum and maximum values in the BST
     Node* minNode = bst.getMin();
     Node* maxNode = bst.getMax();
     std::cout << "Minimum value in the BST: " << minNode->key << std::endl;
     std::cout << "Maximum value in the BST: " << maxNode->key << std::endl;
 
-    // Get height of the BST
     int height = bst.getHeight();
     std::cout << "Height of the BST: " << height << std::endl;
 
-    // Find predecessor and successor of a node
     Node* nodeToFind = bst.search(30);
     Node* predecessorNode = bst.predecessor(nodeToFind);
     Node* successorNode = bst.successor(nodeToFind);
@@ -63,6 +55,14 @@ int main() {
     } else {
         std::cout << "No successor for " << nodeToFind->key << "." << std::endl;
     }
+
+    int deleteValue = 40;
+    std::cout << "Deleting node with value " << deleteValue << " from the BST." << std::endl;
+    bst.remove(deleteValue);
+
+    std::cout << "Inorder Traversal after deletion: ";
+    bst.inorderTraversal();
+    std::cout << std::endl;
 
     return 0;
 }
